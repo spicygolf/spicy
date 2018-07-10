09c47d7ed8e5d55f1f983cfa4c060f97d3270986
Brad Anderson <brad@sankatygroup.com>
2018-07-10 11:27:12 -0400

    remove redux codez

--- src/app/reducers/editingEntitiesReducer.js
+++
@@ -1,7 +0,0 @@
-import {createReducer} from "common/utils/reducerUtils";
-
-import orm from "app/schema";
-const defaultEditingEntities = orm.getEmptyState();
-
-export default createReducer(defaultEditingEntities, {
-});
