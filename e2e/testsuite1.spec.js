5bdd4adf5f9c616debd6b7fbb04468a99e5dd0e5
Brad Anderson <brad@sankatygroup.com>
2019-01-08 19:56:02 -0500

    testing rename, expansion

--- e2e/testsuite1.spec.js
+++ e2e/testsuite1.spec.js
@@ -1,5 +1,16 @@
 describe('SpicyGolf', () => {
-  beforeEach(async () => {
+
+  beforeAll(async () => {
+
+    // for dev - TODO: remove
+    await waitFor(element(by.id('games_tab')))
+      .toBeVisible()
+      .withTimeout(3000);
+
+    return;
+
+
+
     await device.launchApp();
 
     await waitFor(element(by.id('profile_tab')))
@@ -10,10 +21,7 @@ describe('SpicyGolf', () => {
       await element(by.id('logout_button')).tap();
     } catch(e) {}
 
-  });
-
-  it('should be able to login and see app', async () => {
-
+    // login
     await expect(element(by.id('login_form_view'))).toBeVisible();
 
     await element(by.id('email_field'))
@@ -28,12 +36,32 @@ describe('SpicyGolf', () => {
     await element(by.id('login_button'))
       .tap();
 
+  });
+
+  it('should be able to view app', async () => {
+
     await expect(element(by.id('feed_tab')))
       .toBeVisible();
     await expect(element(by.id('games_tab')))
       .toBeVisible();
     await expect(element(by.id('profile_tab')))
       .toBeVisible();
+
+  });
+
+  it('should be able to create new 5 points game', async () => {
+
+    await expect(element(by.id('new_game')))
+      .toBeVisible();
+
+    await element(by.id('new_game'))
+      .tap();
+
+    await element(by.id('new_five_points'))
+      .tap();
+
+    await element(by.id('add_player_button'))
+      .tap();
 
   });
 
