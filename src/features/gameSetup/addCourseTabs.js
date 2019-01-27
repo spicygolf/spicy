e123e164cdb487dc1adf83b66bf5a594dca96680
Brad Anderson <brad@sankatygroup.com>
2019-01-26 23:33:08 -0500

    style the tabs

--- src/features/gameSetup/addCourseTabs.js
+++ src/features/gameSetup/addCourseTabs.js
@@ -1,11 +1,22 @@
+import React from 'react';
 import {
   createMaterialTopTabNavigator
 } from 'react-navigation';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import AddCourseFavorites from 'features/gameSetup/addCourseFavorites';
 import AddCourseSearch from 'features/gameSetup/addCourseSearch';
 
+import { green } from 'common/colors';
 
+
+
+const TabIcon = ({name, color}) => {
+  return (
+    <Icon size={24} color={color} name={name}/>
+  );
+};
 
 const AddCourseTabs = createMaterialTopTabNavigator(
   {
@@ -13,16 +24,43 @@ const AddCourseTabs = createMaterialTopTabNavigator(
       screen: AddCourseFavorites,
       navigationOptions: {
         title: 'Favorites',
+        tabBarIcon: ({ focused, horizontal, tintColor }) => {
+          return (
+            <TabIcon
+              color={focused ? green : '#555' }
+              name='star'
+            />
+          );
+        },
       },
     },
     AddCourseSearch: {
       screen: AddCourseSearch,
       navigationOptions: {
-        title: 'Search'
+        title: 'Search',
+        tabBarIcon: ({ focused, horizontal, tintColor }) => {
+          return (
+            <TabIcon
+              color={focused ? green : '#555' }
+              name='search'
+            />
+          );
+        },
       },
     },
   }, {
     initialRouteName: 'AddCourseFavorites',
+    tabBarOptions: {
+      activeTintColor: green,
+      inactiveTintColor: '#555',
+      style: {
+        backgroundColor: 'none',
+      },
+      indicatorStyle: {
+        backgroundColor: green
+      },
+      showIcon: true
+    }
   }
 );
 
