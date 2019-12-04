57e2a0e662b6ac73366e53b4799f037ca30f2173
Brad Anderson <brad@sankatygroup.com>
2019-12-03 21:17:58 -0500

    upgrade from RN 0.59.9 to 0.61.5

--- android/app/src/main/java/golf/druid/spicygolf/MainActivity.java
+++ android/app/src/main/java/golf/druid/spicygolf/MainActivity.java
@@ -1,4 +1,4 @@
-package com.spicygolf;
+package golf.druid.spicygolf;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -8,8 +8,8 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 public class MainActivity extends ReactActivity {
 
     /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
      */
     @Override
     protected String getMainComponentName() {
