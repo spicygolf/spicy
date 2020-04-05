5b04e71e5e29b684bf44168e8a8cd31248a5088d
Brad Anderson <brad@sankatygroup.com>
2020-04-05 08:05:02 -0400

    take away the header in the GamesStack as it's in the bottom nav

--- src/features/games/gamesstack.js
+++ src/features/games/gamesstack.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {
+  SafeAreaView
+} from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Game from 'features/game/game';
 import Games from 'features/games/games';
 import NewGame from 'features/games/newGame';
-
-import { green } from 'common/colors';
 
 
 
@@ -14,25 +15,22 @@ const GamesStack = props => {
   const Stack = createStackNavigator();
 
   return (
-    <Stack.Navigator
-      initialRouteName='Games'
-      screenOptions={{
-        title: 'Games',
-        headerLeft: null,
-        headerStyle: {
-          backgroundColor: green,
-        },
-        headerTitleStyle: {
-          fontSize: 18,
-          fontWeight: 'bold'
-        },
-        headerTintColor: 'white'
-      }}
-    >
-      <Stack.Screen name='Games' component={Games} />
-      <Stack.Screen name='Game' component={Game} />
-      <Stack.Screen name='NewGame' component={NewGame} />
-    </Stack.Navigator>
+    <SafeAreaView style={{flex: 1,}}>
+      <Stack.Navigator
+        initialRouteName='Games'
+        screenOptions={{
+          title: 'Games',
+          headerMode: 'none',
+          headerStyle: {
+            height: 0,
+          },
+        }}
+      >
+        <Stack.Screen name='Games' component={Games} />
+        <Stack.Screen name='Game' component={Game} />
+        <Stack.Screen name='NewGame' component={NewGame} />
+      </Stack.Navigator>
+    </SafeAreaView>
   );
 
 };
