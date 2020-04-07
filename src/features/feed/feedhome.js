a002326c07a5e4bb16c2118c22943c8ce67f4bdc
Brad Anderson <brad@sankatygroup.com>
2020-04-07 17:38:15 -0400

    pretty up the non-functioning tabs for now

--- src/features/feed/feedhome.js
+++ src/features/feed/feedhome.js
@@ -4,15 +4,23 @@ import {
   Text,
   View,
 } from 'react-native';
+import {
+  Card
+} from 'react-native-elements';
 
 
 
 const FeedHome = props => {
 
   return (
-    <View>
-      <Text>Feed Home</Text>
-    </View>
+    <Card>
+      <Text>
+        The feed will be full of happenings and spicy moments on the course.
+        Even if you're trapped at the office, you can follow all the action.
+      </Text>
+      <Text></Text>
+      <Text>Coming soon...</Text>
+    </Card>
   );
 
 };
--- src/features/profile/profilehome.js
+++ src/features/profile/profilehome.js
@@ -1,32 +1,37 @@
 'use strict';
 
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   StyleSheet,
-  View
+  Text,
+  View,
 } from 'react-native';
 import { useApolloClient } from '@apollo/react-hooks';
 import {
+  Card,
   Button
 } from 'react-native-elements';
 
 import { red } from 'common/colors';
 import { logout } from 'common/utils/account';
+import { CurrentPlayerContext } from 'features/players/currentPlayerContext';
+
 
 
 const ProfileHome = props => {
 
   const client = useApolloClient();
+  const { currentPlayer } = useContext(CurrentPlayerContext);
 
   const _logoutPressed = () => {
     logout(client);
   }
 
+/*
   const _clearCache = props => {
     client.resetStore();
   }
 
-/*
       <Button
         style={styles.logout_button}
         title='Clear Cache'
@@ -35,14 +40,24 @@ const ProfileHome = props => {
       />
 */
   return (
-    <View>
-      <Button
-        style={styles.logout_button}
-        title='Logout'
-        testID='logout_button'
-        onPress={() => _logoutPressed()}
-      />
-    </View>
+    <Card>
+      <View style={styles.field_view}>
+        <Text>Name:</Text>
+        <Text>{currentPlayer.name}</Text>
+      </View>
+      <View style={styles.field_view}>
+        <Text>Current Index:</Text>
+        <Text>{currentPlayer.handicap.index || null}</Text>
+      </View>
+      <View style={styles.button_view}>
+        <Button
+          style={styles.logout_button}
+          title='Logout'
+          testID='logout_button'
+          onPress={() => _logoutPressed()}
+        />
+      </View>
+    </Card>
   );
 
 };
@@ -51,7 +66,13 @@ export default ProfileHome;
 
 
 const styles = StyleSheet.create({
+  field_view: {
+    marginVertical: 10,
+  },
+  button_view: {
+    marginTop: 40,
+  },
   logout_button: {
     margin: 20
-  }
+  },
 });
