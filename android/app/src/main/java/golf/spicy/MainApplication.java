6225da1502cba3fab67cda5570525f94c57bfc42
Brad Anderson <brad@sankatygroup.com>
2020-04-15 08:30:27 -0400

    Android build stuffs, remove flipper

--- android/app/src/main/java/golf/spicy/MainApplication.java
+++ android/app/src/main/java/golf/spicy/MainApplication.java
@@ -45,36 +45,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
-  /**
-   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-   *
-   * @param context
-   */
-  private static void initializeFlipper(
-    Context context, ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("golf.spicy.ReactNativeFlipper");
-        aClass
-          .getMethod("initializeFlipper", Context.class, reactInstanceManager.class)
-          .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
-    }
-  }
 }
