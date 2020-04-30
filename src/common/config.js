f420969c75c33a9157dd4a44bdf7a00e0321c219
Brad Anderson <brad@sankatygroup.com>
2020-04-29 20:30:04 -0400

    switch to API v2

--- src/common/config.js
+++ src/common/config.js
@@ -2,9 +2,9 @@
 'use strict';
 
 //export const scheme = 'https';
-//export const baseUri = 'api.spicy.golf/v1';     //prod
+//export const baseUri = 'api.spicy.golf/v2';     //prod
 
 
 export const scheme = 'http';
-export const baseUri = 'localhost:3010/v1';  // ios
+export const baseUri = 'localhost:3010/v2';  // ios
 //export const baseUri = '10.0.2.2:3010/v1';   // android
