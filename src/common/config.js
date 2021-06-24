0d8f26f23d7bb4af6b3c658e51f2a69f74c9d251
Brad Anderson <brad@sankatygroup.com>
2021-06-24 10:48:37 -0400

    API version bump for options refactor

--- src/common/config.js
+++ src/common/config.js
@@ -1,17 +1,17 @@
 
-const env = 'PROD';
+const env = 'DEV';
 
 let s, b;
 
 switch( env ) {
   case 'PROD':
     s = 'https';                 // prod
-    b = 'api.spicy.golf/v2';     // prod
+    b = 'api.spicy.golf/v3';     // prod
     break;
   case 'DEV':
     s = 'http';                  // dev
-    b = 'localhost:3010/v2';     // dev - ios
-    // b = '10.0.2.2:3010/v2';     // dev - android
+    b = 'localhost:3010/v3';     // dev - ios
+    // b = '10.0.2.2:3010/v3';     // dev - android
 }
 
 export const scheme = s;
