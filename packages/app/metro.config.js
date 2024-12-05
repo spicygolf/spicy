b2f698a44e783cafa6e95c73bd10556ba648c15b
Brad Anderson <brad@sankatygroup.com>
2024-12-04 21:56:02 -0500

    mv app-expo, create app

--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -1,6 +1,11 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-const config = getDefaultConfig(__dirname);
-config.resolver.unstable_enablePackageExports = true
-module.exports = withNativeWind(config, { input: "./src/global.css" });
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
