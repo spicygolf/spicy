ae6f8121a9de684cea7b5abf5113da267ba506e7
Brad Anderson <brad@sankatygroup.com>
2025-06-23 21:22:05 -0400

    chore: upgrade RN 0.76.3 -> 0.80.0

--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -9,7 +9,7 @@ const yalcNodeModules = path.resolve(__dirname, '.yalc');
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {
   resolver: {
