ac79a2b63fe20e84fff5eac05c342f74a8dcd0f0
Brad Anderson <brad@sankatygroup.com>
2025-07-07 22:54:42 -0400

    fix: NewGame now properly updates GameContext

--- packages/app/src/navigators/GameNavigator.tsx
+++ packages/app/src/navigators/GameNavigator.tsx
@@ -3,7 +3,6 @@ import {
   createNativeStackNavigator,
   type NativeStackScreenProps,
 } from '@react-navigation/native-stack';
-import { GameProvider } from '@/providers/game';
 import { GameScoring } from '@/screens/game/GameScoring';
 import { GameSettings } from '@/screens/game/GameSettings';
 
@@ -25,25 +24,23 @@ export function GameNavigator() {
   const Stack = createNativeStackNavigator<GameNavigatorParamList>();
 
   return (
-    <GameProvider>
-      <Stack.Navigator initialRouteName="GameSettings">
-        <Stack.Screen
-          name="GameSettings"
-          component={GameSettings}
-          options={{
-            title: 'Settings',
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="GameScoring"
-          component={GameScoring}
-          options={{
-            title: 'Scoring',
-            headerShown: false,
-          }}
-        />
-      </Stack.Navigator>
-    </GameProvider>
+    <Stack.Navigator initialRouteName="GameSettings">
+      <Stack.Screen
+        name="GameSettings"
+        component={GameSettings}
+        options={{
+          title: 'Settings',
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="GameScoring"
+        component={GameScoring}
+        options={{
+          title: 'Scoring',
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
   );
 }
--- packages/app/src/screens/game/GameSettings.tsx
+++ packages/app/src/screens/game/GameSettings.tsx
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react';
 import { ActivityIndicator, View } from 'react-native';
 import { GameSettingsPlayers } from '@/components/game/settings/GameSettingsPlayers';
+import type { GameSettingsProps } from '@/navigators/GameNavigator';
 import { GameContext } from '@/providers/game';
 import { Screen, Text } from '@/ui';
 
-export function GameSettings(props) {
+export function GameSettings(props: GameSettingsProps) {
   console.log('GameSettings props', props);
 
   const { game } = useContext(GameContext);
+  console.log('GameSettings game', game);
   if (!game)
     return (
       <Screen>
