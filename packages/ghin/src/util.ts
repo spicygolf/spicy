dde11dc80efbe77a5c9d21ed6d65abd5b40d3c2f
Brad Anderson <brad@sankatygroup.com>
2025-08-09 20:59:47 -0400

    chore: remove homegrown GHIN library

--- packages/ghin/src/util.ts
+++
@@ -1,8 +0,0 @@
-import { Gender } from "./types";
-
-export const getGenderEnum = (gender?: string): Gender => {
-  if (!gender) {
-    return Gender.Male;
-  }
-  return gender in ["F", "f", "Female", "female"] ? Gender.Female : Gender.Male;
-};
