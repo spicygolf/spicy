e74ca5636177f5001bedc575cefd078cf123f51b
Brad Anderson <brad@sankatygroup.com>
2025-10-04 11:07:00 -0400

    fix: do more typescript checking

--- packages/app/src/components/Back.tsx
+++ packages/app/src/components/Back.tsx
@@ -1,11 +1,16 @@
 import FontAwesome6 from "@react-native-vector-icons/fontawesome6";
+import type { ParamListBase } from "@react-navigation/native";
 import { UnistylesRuntime } from "react-native-unistyles";
 import type { Href } from "@/ui/Link";
 import { Link } from "@/ui/Link";
 
-type Props = { home: Href };
+type Props<ParamList extends ParamListBase = ParamListBase> = {
+  home: Href<ParamList>;
+};
 
-export function Back({ home }: Props) {
+export function Back<ParamList extends ParamListBase = ParamListBase>({
+  home,
+}: Props<ParamList>) {
   const theme = UnistylesRuntime.getTheme();
   return (
     <Link href={home}>
